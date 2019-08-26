93d19061a50d748f396e36eba2636a4c28026674
Drew <verwiel.code@gmail.com>
2019-08-25 21:31:20 -0600

    server disconnect v2

--- server.js
+++ server.js
@@ -14,27 +14,51 @@ const db_config = {
   database : process.env.DB_NAME
 }
 
-handleDisconnect() {
-  connection = mysql.createConnection(db_config)
+var connection;
 
-  connection.connect(err => {
-  if(err) {
-    console.log('error connecting to db: ', err)
-    setTimeout(handleDisconnect, 2000)
-  }
-})
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
 
-connection.on('error', (err) => {
-  console.log('database error', err);
-  if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-    handleDisconnect()
-  } else {                                      
-    throw err                            
-  }
-})
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
 }
 
-handleDisconnect()
+handleDisconnect();
+// handleDisconnect() {
+//   connection = mysql.createConnection(db_config);
+
+//   connection.connect(err => {
+//   if(err) {
+//     console.log('error connecting to db: ', err);
+//     setTimeout(handleDisconnect, 2000);
+//   }
+// });
+
+// connection.on('error', (err) => {
+//   console.log('database error', err);
+//   if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//     handleDisconnect()
+//   } else {                                      
+//     throw err;              
+//   }
+// });
+// }
+
+// handleDisconnect()
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
